071d3068068063566c4e0811009ef8bf0669d459
Leo Wilhelm Lierse <lierseleow@gmail.com>
2023-07-11 11:54:06 +0200

    changed the thermostat startup script

--- sensor/startup_thermostats.ps1
+++ sensor/startup_thermostats.ps1
@@ -1,2 +1,2 @@
-start powershell {$env:THERMOSTAT_SERVER_PORT = 24241; python -m thermostat.thermostat}
-start powershell {$env:THERMOSTAT_SERVER_PORT = 24242; python -m thermostat.thermostat}
+start powershell {$env:THERMOSTAT_SERVER_PORT = 24241; python -m thermostat.thermostat > thermo_log_24241.txt}
+start powershell {$env:THERMOSTAT_SERVER_PORT = 24242; python -m thermostat.thermostat > thermo_log_24242.txt}
